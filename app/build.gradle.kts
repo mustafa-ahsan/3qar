f21a9ef991f695892679269dbf50be0f0502313d
mustafa-ahsan <44897579+mustafa-ahsan@users.noreply.github.com>
2026-09-07 19:27:14 +0000

    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
